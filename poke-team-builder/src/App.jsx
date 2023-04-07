e51f92588a3d41c44d871b75fd0c9b1f6c610dc1
tkoenig97 <tkoenig97@github.com>
2023-04-07 15:08:57 -0500

    Commented out index.css ---- might delete

--- poke-team-builder/src/App.jsx
+++ poke-team-builder/src/App.jsx
@@ -1,15 +1,15 @@
 import { Header } from './components/Header';
 import { Outlet } from "react-router-dom";
 import './App.css';
-import Container from "react-bootstrap/Container";
 
 function App() {
 
     return (
-        <Container className="App">
-          <Header />
+        <>
+            <Header />
             <Outlet />
-        </Container>
+        </>
+
       );
 }
 
