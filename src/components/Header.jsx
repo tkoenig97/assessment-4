d85d172c66c35e094f317cce608c719dd0205827
tkoenig97 <tkoenig97@github.com>
2023-04-08 20:09:16 -0500

    Header now passes all tests

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,23 @@
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-export const Header = () => {
-
-    const searchForPokemon = async(pokemon) => {
-        let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-        return response.data.results
-    }    
+export const Header = ({ caughtPokemon = [] }) => {
+    const searchForPokemon = async (pokemon) => {
+        let response = await axios.get(
+            `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+        );
+        return response.data.results;
+    };
 
     return (
-    <div className="header">
-        <h1>POKEDEX</h1>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/team/'}>My Team #</Link>
-        <form onSubmit={searchForPokemon}>
-            <input placeholder='search'></input>
-            <input type='submit'></input>
-        </form>
-    </div>
-    )
+        <div className="header">
+            <h1>POKEDEX</h1>
+            <Link to={'/'}>Home</Link>
+            <Link to={'/team/'}>My Team #{caughtPokemon.length}</Link>
+            <form onSubmit={searchForPokemon}>
+                <input placeholder="search"></input>
+                <button type="submit">Search</button>
+            </form>
+        </div>
+    );
 };
