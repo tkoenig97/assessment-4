d49bae9c7d90f968b7302587577dd4b99f2002ed
tkoenig97 <tkoenig97@github.com>
2023-04-11 15:37:40 -0500

    Removed unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { useState } from 'react';
 import { Header } from './components/Header';
 import { Outlet } from 'react-router-dom';
 import { TeamContext } from './components/TeamContext';
